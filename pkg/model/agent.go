666f2f7cfc1b186ece515bf41c346015da8cc481
Miguel DA SILVA <miguel@dasilvaf.net>
2025-05-21 16:06:29 +0200

    create agent model

---
+++ pkg/model/agent.go
@@ -0,0 +1,26 @@
+package model
+
+type AgentRegister struct {
+	Data struct {
+		Token    string   `json:"token"`
+		Agent    Agent    `json:"agent"`
+		Faction  Faction  `json:"faction"`
+		Contract Contract `json:"contract"`
+		Ships    []Ship   `json:"ships"`
+	} `json:"data"`
+}
+
+type AgentData struct {
+	Data struct {
+		Agent
+	} `json:"data"`
+}
+
+type Agent struct {
+	AccountID       string `json:"accountId,omitempty"`
+	Symbol          string `json:"symbol"`
+	Headquarters    string `json:"headquarters"`
+	Credits         int64  `json:"credits"`
+	StartingFaction string `json:"startingFaction"`
+	ShipCount       int64  `json:"shipCount"`
+}
