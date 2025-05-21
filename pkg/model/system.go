e1ba816d04caceb772aff843778da4c8fc82f1cb
Miguel Da Silva <miguel@dasilvaf.net>
2025-05-21 17:42:55 +0200

    [pkg/model]: Format system.go

--- pkg/model/system.go
+++ pkg/model/system.go
@@ -2,29 +2,25 @@ package model
 
 type System struct {
 	Constellation string `json:"constellation"`
-	Symbol string `json:"symbol"`
-	SectorSymbol string `json:"sectionSymbol"`
-	Type string `json:"type"`
-	X int `json:"x"`
-	Y int `json:"y"`
-	Waypoints []SystemWaypoint
-	Factions []SystemFaction
-	Name string `json:"name"`
+	Symbol        string `json:"symbol"`
+	SectorSymbol  string `json:"sectionSymbol"`
+	Type          string `json:"type"`
+	X             int    `json:"x"`
+	Y             int    `json:"y"`
+	Waypoints     []SystemWaypoint
+	Factions      []SystemFaction
+	Name          string `json:"name"`
 }
 
 type SystemWaypoint struct {
-	Symbol string `json:"symbol"`
-	Type string `json:"type"`
-	X int `json:"x"`
-	Y int `json:"y"`
+	Symbol   string `json:"symbol"`
+	Type     string `json:"type"`
+	X        int    `json:"x"`
+	Y        int    `json:"y"`
 	Orbitals []WaypointOrbital
-	Orbits string `json:"orbits"`
+	Orbits   string `json:"orbits"`
 }
 
 type SystemFaction struct {
-	Symbol string `json:"symbol"`
-}
-
-type WaypointOrbital struct {
 	Symbol string `json:"symbol"`
 }
