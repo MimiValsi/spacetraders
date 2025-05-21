2c0853d8d34d4ccb48b8a667b6698ca9effbf907
Miguel Da Silva <miguel@dasilvaf.net>
2025-05-21 17:45:50 +0200

    [test]: test api.NewClient and Register functions

--- cmd/spacetraders/main.go
+++ cmd/spacetraders/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/MimiValsi/spacetraders/pkg/api"
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	godotenv.Load()
+
+	accountToken := os.Getenv("ACCOUNT")
+	if accountToken == "" {
+		log.Fatalln("Must set ACCOUNT token")
+	}
+
+	client, err := api.NewClient(context.Background(), accountToken)
+	if err != nil {
+		log.Fatalln("new client error")
+	}
+
+	if err := client.Register("MIMI2", "AEGIS"); err != nil {
+		log.Fatalln(err)
+	}
 
 }
