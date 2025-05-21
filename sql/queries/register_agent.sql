d9e94c7a1d571f156e37cd05305c2044f5687bed
Miguel Da Silva <miguel@dasilvaf.net>
2025-05-21 21:38:38 +0200

    [pkg/sql]: create register agent query. Need test

---
+++ sql/queries/register_agent.sql
@@ -0,0 +1,5 @@
+-- name: registerAgent :one
+INSERT INTO agents (account_id, token, symbol, headquarters, credits, starting_faction, ship_count)
+VALUES ($1, $2, $3, $4, $5, $6, $7)
+RETURNING *;
+--
