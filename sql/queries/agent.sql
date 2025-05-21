08da9a018266a0fcbe1b04634fbce38dd2f2d748
Miguel Da Silva <miguel@dasilvaf.net>
2025-05-21 22:41:24 +0200

    [misc]: change pkg/sql/queries file name

--- sql/queries/agent.sql
+++ sql/queries/agent.sql
@@ -3,3 +3,9 @@ INSERT INTO agents (account_id, token, symbol, headquarters, credits, starting_f
 VALUES ($1, $2, $3, $4, $5, $6, $7)
 RETURNING *;
 --
+
+-- name: registerFaction :one
+INSERT INTO factions (symbol, name, description, headquarters, traits_id, is_recruiting)
+VALUES ($1, $2, $3, $4, $5, $6)
+RETURNING *;
+--
