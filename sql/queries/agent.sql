be09dc83a525d33b26c75e95e4a567eb61840277
Miguel DA SILVA <miguel@dasilvaf.net>
2025-05-22 14:16:38 +0200

    [pkg/sql]: Add id return from query and move faction query to another file.

--- sql/queries/agent.sql
+++ sql/queries/agent.sql
@@ -1,11 +1,6 @@
 -- name: registerAgent :one
 INSERT INTO agents (account_id, token, symbol, headquarters, credits, starting_faction, ship_count)
 VALUES ($1, $2, $3, $4, $5, $6, $7)
-RETURNING *;
+RETURNING id;
 --
 
--- name: registerFaction :one
-INSERT INTO factions (symbol, name, description, headquarters, traits_id, is_recruiting)
-VALUES ($1, $2, $3, $4, $5, $6)
-RETURNING *;
---
