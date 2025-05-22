0c1df8bfeb1497c9c48af1173bd61f258f6bb4b6
Miguel DA SILVA <miguel@dasilvaf.net>
2025-05-22 14:20:12 +0200

    [misc]: Format

--- sql/queries/agent.sql
+++ sql/queries/agent.sql
@@ -1,5 +1,13 @@
 -- name: registerAgent :one
-INSERT INTO agents (account_id, token, symbol, headquarters, credits, starting_faction, ship_count)
+INSERT INTO agents (
+  account_id,
+  token,
+  symbol,
+  headquarters,
+  credits,
+  starting_faction,
+  ship_count
+)
 VALUES ($1, $2, $3, $4, $5, $6, $7)
 RETURNING id;
 --
