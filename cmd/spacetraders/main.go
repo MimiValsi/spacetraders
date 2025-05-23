71df387b9cd26826422110de72afe2823b2e99cf
Miguel DA SILVA <miguel@dasilvaf.net>
2025-05-23 10:56:19 +0200

    [main]: test newly saveToDB func. It works!!!

--- cmd/spacetraders/main.go
+++ cmd/spacetraders/main.go
@@ -2,27 +2,43 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
 	"os"
 
+	_ "github.com/lib/pq"
+
+	"github.com/MimiValsi/spacetraders/internal/database"
 	"github.com/MimiValsi/spacetraders/pkg/api"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	godotenv.Load()
+	_ = godotenv.Load()
 
 	accountToken := os.Getenv("ACCOUNT")
 	if accountToken == "" {
 		log.Fatalln("Must set ACCOUNT token")
 	}
 
-	client, err := api.NewClient(context.Background(), accountToken)
+	dbURL := os.Getenv("dbURL")
+	if dbURL == "" {
+		log.Fatalln("Must set dbURL token")
+	}
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("couldn't open DB: %s", err)
+	}
+
+	queries := database.New(db)
+
+	client, err := api.NewClient(context.Background(), accountToken, queries)
 	if err != nil {
 		log.Fatalln("new client error")
 	}
 
-	if err := client.Register("MIMI3", "AEGIS"); err != nil {
+	if err = client.Register("MIMI8", "AEGIS"); err != nil {
 		log.Fatalln(err)
 	}
 
