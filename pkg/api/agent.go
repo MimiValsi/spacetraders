047db4dc77917b90fce07c9ddceff19bd6cf5ac4
Miguel DA SILVA <miguel@dasilvaf.net>
2025-05-24 15:03:14 +0200

    [pkg/api]: start agent loading functions.

--- pkg/api/agent.go
+++ pkg/api/agent.go
@@ -1,3 +1,28 @@
 package api
 
-import ()
+import (
+	"context"
+	"github.com/MimiValsi/spacetraders/internal/database"
+)
+
+type Agent struct {
+	Token   string
+	Credits int64
+	Headquarters string
+
+	DB *database.Queries
+}
+
+func LoadAgent(db *database.Queries) (*Agent, error) {
+	agent, err := db.GetAgent(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	return &Agent{
+		Token: agent.Token,
+		Credits: agent.Credits,
+		Headquarters: agent.Headquarters,
+		DB: db,
+	}, nil
+}
